60448e63181d77952c1949d8730d68104a7c006b
nguyenducde <ducde678@gmail.com>
2020-11-13 07:53:17 +0700

    <3

--- LayNoiDungWebSite/index.php
+++ LayNoiDungWebSite/index.php
@@ -1,9 +1,11 @@
-<?php
-$data = file_get_contents('https://vi.wikipedia.org/wiki/%C4%90%E1%BA%A1i_d%E1%BB%8Bch_COVID-19') or die('Không đọc được dữ liệu');
-$pattern='#(?<=<table class="wikitable plainrowheaders sortable jquery-tablesorter" style="text-align:right; font-size:90%; width:390px; float:right; clear:right; margin:0px 0px 0.5em 1em;">
->).*(?=</table>)';
-preg_match($pattern, $data, $matches);
-$result='<table boder="1px"' . $matches[0] . '</table>';
-echo $result;
-?>
+<?php 
+    include('simple_html_dom.php');
+    $url = 'https://spiderum.com/bai-dang/KHONG-CAN-DAY-NHAU-TU-DUY-uus';
+    $html = file_get_html($url);
+    foreach($html->find('img') as $element) {
+        echo '<img src="'.$element->src.'" /><br>';
+ }
+//  $noidung = $html->find('#post-content-view-edit',0);
+//  echo $noidung;
 
+?>
