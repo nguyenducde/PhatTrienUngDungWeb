ee50d10f133d08268027b85461d8beba64ddaac7
nguyenducde <ducde678@gmail.com>
2020-11-13 08:13:29 +0700

    <3

--- LayNoiDungWebSite/index.php
+++ LayNoiDungWebSite/index.php
@@ -2,10 +2,11 @@
     include('simple_html_dom.php');
     $url = 'https://spiderum.com/bai-dang/KHONG-CAN-DAY-NHAU-TU-DUY-uus';
     $html = file_get_html($url);
-    foreach($html->find('img') as $element) {
-        echo '<img src="'.$element->src.'" /><br>';
- }
-//  $noidung = $html->find('#post-content-view-edit',0);
-//  echo $noidung;
+    
+//     foreach($html->find('img') as $element) {
+//         echo '<img src="'.$element->src.'" /><br>';
+//  }
+ $noidung = $html->find('#post-content',0);
+ echo $noidung;
 
 ?>
